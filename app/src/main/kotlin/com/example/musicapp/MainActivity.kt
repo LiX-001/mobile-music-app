56330fcf411f9ceeac4e4d91365aac62f12f3429
LiX <103515763+LiX-001@users.noreply.github.com>
2025-02-28 15:31:45 +0000

    Auto commit

--- app/src/main/kotlin/com/example/musicapp/MainActivity.kt
+++ app/src/main/kotlin/com/example/musicapp/MainActivity.kt
@@ -11,12 +11,12 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
 
         Log.d("DEBUG", "MainActivity started")  // Log when activity starts
         Toast.makeText(this, "App Started", Toast.LENGTH_LONG).show()
         
-
+        setContentView(R.layout.activity_main)
         // UI elements
         val textView: TextView = findViewById(R.id.textView1)
         val button: Button = findViewById(R.id.button1)
