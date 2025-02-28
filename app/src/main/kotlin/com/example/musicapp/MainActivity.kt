8b404ad36f9fd45279adab5ff88206c11fdaccf0
LiX <103515763+LiX-001@users.noreply.github.com>
2025-02-28 15:43:56 +0000

    Auto commit

--- app/src/main/kotlin/com/example/musicapp/MainActivity.kt
+++ app/src/main/kotlin/com/example/musicapp/MainActivity.kt
@@ -11,12 +11,18 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+        try {
+            setContentView(R.layout.activity_main)
+            Log.d("DEBUG", "setContentView() is working.")
+            Toast.makeText(this, "setContentView() is working.", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Log.d("DEBUG", "error in setContentView() : ", e)  // Log when activity starts
+            Toast.makeText(this, "error in setContentView() : "+e, Toast.LENGTH_LONG).show()
+        }
 
         Log.d("DEBUG", "MainActivity started")  // Log when activity starts
         Toast.makeText(this, "App Started", Toast.LENGTH_LONG).show()
         
-        setContentView(R.layout.activity_main)
         // UI elements
         val textView: TextView = findViewById(R.id.textView1)
         val button: Button = findViewById(R.id.button1)
