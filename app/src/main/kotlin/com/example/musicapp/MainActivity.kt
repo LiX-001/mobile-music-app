991eeab972e996cac5c16abbfcf5401ebe5a40f8
LiX <103515763+LiX-001@users.noreply.github.com>
2025-02-28 15:56:52 +0000

    Auto commit

--- app/src/main/kotlin/com/example/musicapp/MainActivity.kt
+++ app/src/main/kotlin/com/example/musicapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.musicapp
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 
 import android.widget.Button
 import android.widget.TextView
@@ -17,7 +20,10 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "setContentView() is working.", Toast.LENGTH_LONG).show()
         } catch (e: Exception) {
             Log.d("DEBUG", "error in setContentView() : ", e)  // Log when activity starts
-            Toast.makeText(this, "error in setContentView() : "+e, Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Error message copied to clipboard.", Toast.LENGTH_LONG).show()
+
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("Error Log", e.toString())
         }
 
         Log.d("DEBUG", "MainActivity started")  // Log when activity starts
